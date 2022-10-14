33d8d2703b04a7ceb1e76eb729e7e58d560bf764
olaf michaelis <o.mic@web.de>
2022-10-14 10:23:52 +0200

    Add print layouts as table

--- go.mod
+++ go.mod
@@ -5,8 +5,17 @@ go 1.19
 require github.com/alecthomas/kong v0.6.1
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
 	github.com/gandarez/go-olson-timezone v0.1.0 // indirect
+	github.com/go-openapi/errors v0.20.2 // indirect
+	github.com/go-openapi/strfmt v0.21.3 // indirect
 	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/yookoala/realpath v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
 	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
 )
--- src/dparser/output.go
+++ src/dparser/output.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/jedib0t/go-pretty/table"
 )
 
 func (dp DParser) PrintDiffJSON() {
@@ -16,8 +19,49 @@ func (dp DParser) PrintDiffJSON() {
 
 func (dp DParser) ListSupportedFormats() {
 	fmt.Printf("\nThe following date layouts are supported\n\n")
+	t := initTable()
+	t.AppendHeader(table.Row{"Format", "Layout", "Comment"})
 	for _, el := range dp.Layouts {
-		fmt.Printf("%22s |%s\n", el.Layout, el.Matcher)
+		t.AppendRow(
+			[]interface{}{
+				el.Desc, el.Layout, el.Comment,
+			},
+		)
 	}
 	fmt.Printf("\n")
+	t.Render()
+}
+
+func initTable() (t table.Writer) {
+	t = table.NewWriter()
+	t.SetStyle(table.Style{
+		Name: "myNewStyle",
+		Box: table.BoxStyle{
+			BottomLeft:       "\\",
+			BottomRight:      "/",
+			BottomSeparator:  "v",
+			Left:             "[",
+			LeftSeparator:    "{",
+			MiddleHorizontal: "-",
+			MiddleSeparator:  "+",
+			MiddleVertical:   "|",
+			PaddingLeft:      " ",
+			PaddingRight:     " ",
+			Right:            " ]",
+			RightSeparator:   "}",
+			TopLeft:          "(",
+			TopRight:         ")",
+			TopSeparator:     "^",
+			UnfinishedRow:    " ~~~",
+		},
+		Options: table.Options{
+			DrawBorder:      false,
+			SeparateColumns: true,
+			SeparateFooter:  true,
+			SeparateHeader:  true,
+			SeparateRows:    false,
+		},
+	})
+	t.SetOutputMirror(os.Stdout)
+	return
 }
